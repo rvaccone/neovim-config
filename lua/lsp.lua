44baad8b028ff3041e0b425e4658b624e2da2812
Rocco Vaccone <rocco@vaccone.us>
2024-12-27 01:56:47 -0500

    Switched to using conform for prettier instead of vim-prettier

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -8,6 +8,21 @@ local M = {}
 
 ---@return nil
 function M.setup()
+	-- Configure diagnostics display
+	vim.diagnostic.config({
+		virtual_text = true,
+		signs = false,
+		underline = true,
+		update_in_insert = false,
+		severity_sort = true,
+		float = {
+			border = "rounded",
+			source = "always",
+			header = "",
+			prefix = "",
+		},
+	})
+
 	--  Setup Mason
 	require("mason").setup({})
 	require("mason-lspconfig").setup({
--- lua/plugins/stevearc#conform.nvim.lua
+++ lua/plugins/stevearc#conform.nvim.lua
@@ -1,13 +1,28 @@
 return {
 	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
 	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "black" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			css = { "prettier" },
+			html = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			graphql = { "prettier" },
+			["*"] = { "trim_whitespace" },
+			["_"] = { "trim_newlines" },
 		},
 		format_on_save = {
-			lsp_format = "fallback",
 			timeout_ms = 500,
+			lsp_format = "fallback",
 		},
+		notify_on_error = true,
 	},
 }
